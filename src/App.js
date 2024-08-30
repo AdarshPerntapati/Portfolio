3e98e32790997c30a2446a8d636e943d2ed82303
AdarshPerntapati <49777475+AdarshPerntapati@users.noreply.github.com>
2024-08-31 04:45:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div class='h1 bg-dark text-white'>
+      <h1>This is a new project DAY-1</h1>
+    </div>
+  );
+}
+
+export default App;
